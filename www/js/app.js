69b22da694eb7b3797c6375cf5ec745c2fd6bc4b
junyan-zg <275620888@qq.com>
2016-03-19 07:51:26 +0800

    取消app左右功能

--- www/js/app.js
+++ www/js/app.js
@@ -13,42 +13,29 @@
 angular.module('starter', ['ionic','ngCordova','starter.controllers'])
 
 .run(function($ionicPlatform, $rootScope, $location, $timeout, $ionicHistory,$cordovaToast,$cordovaKeyboard,$ionicPopup) {
-  $ionicPlatform.ready(function() {
+    $ionicPlatform.ready(function() {
 
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-   //双击退出
-        $ionicPlatform.registerBackButtonAction(function (e) {
-            if (ionic.keyboard.isOpen) {
-                $cordovaKeyboard.close();
-            }
-            //判断处于哪个页面时双击退出
-            else if ($location.path() == '/tab') {
-                if ($rootScope.backButtonPressedOnceToExit) {
-                    ionic.Platform.exitApp();
-                } else {
-                    $rootScope.backButtonPressedOnceToExit = true;
-                    $cordovaToast.showShortBottom('再按一次退出系统');
-                    setTimeout(function () {
-                        $rootScope.backButtonPressedOnceToExit = false;
-                    }, 2000);
-                }
-            }
-            else if ($ionicHistory.backView()) {
-                if (ionic.keyboard.isOpen) {
-                    $cordovaKeyboard.close();
-                } else {
-                    $ionicHistory.goBack();
-                }
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
+    //双击退出
+    $ionicPlatform.registerBackButtonAction(function (e) {
+        if (ionic.keyboard.isOpen) {
+            $cordovaKeyboard.close();
+        }
+        //判断处于哪个页面时双击退出
+        else if ($location.path() == '/tab/cook' || $location.path() == '/tab/business' ||
+            $location.path() == '/tab/share' || $location.path() == '/tab/user') {
+            if ($rootScope.backButtonPressedOnceToExit) {
+                ionic.Platform.exitApp();
             } else {
                 $rootScope.backButtonPressedOnceToExit = true;
                 $cordovaToast.showShortBottom('再按一次退出系统');
@@ -56,47 +43,61 @@ angular.module('starter', ['ionic','ngCordova','starter.controllers'])
                     $rootScope.backButtonPressedOnceToExit = false;
                 }, 2000);
             }
-            e.preventDefault();
-            return true;
-        }, 101);
-   /* //弹出窗口退出
-        $ionicPlatform.registerBackButtonAction(function (e) {
-            function showConfirm() {
-                var confirmPopup = $ionicPopup.confirm({
-                    title: '<strong>退出应用?</strong>',
-                    template: '你确定要退出应用吗?',
-                    okText: '退出',
-                    cancelText: '取消'
-                });
-                confirmPopup.then(function (res) {
-                    if (res) {
-                        ionic.Platform.exitApp();
-                    } else {
-                        // Don't close
-                    }
-                });
+        }
+        else if ($ionicHistory.backView()) {
+            if (ionic.keyboard.isOpen) {
+                $cordovaKeyboard.close();
+            } else {
+                $ionicHistory.goBack();
             }
-            //判断处于哪个页面时双击退出
-            if ($location.path() == '/tab' ) {
-                if (ionic.keyboard.isOpen) {
-                    $cordovaKeyboard.close();
-                }else{
-                    showConfirm();
-                }
-            }else if ($ionicHistory.backView()) {
-                if (ionic.keyboard.isOpen) {
-                    $cordovaKeyboard.close();
-                }else{
-                    $ionicHistory.goBack();
-                }
-            }else {
-                // This is the last page: Show confirmation popup
-                showConfirm();
-            }
-            e.preventDefault();
-            return false;
-        }, 101);
-*/
+        } else {
+            $rootScope.backButtonPressedOnceToExit = true;
+            $cordovaToast.showShortBottom('再按一次退出系统');
+            setTimeout(function () {
+                $rootScope.backButtonPressedOnceToExit = false;
+            }, 2000);
+        }
+        e.preventDefault();
+        return true;
+    }, 101);
+    /* //弹出窗口退出
+     $ionicPlatform.registerBackButtonAction(function (e) {
+     function showConfirm() {
+     var confirmPopup = $ionicPopup.confirm({
+     title: '<strong>退出应用?</strong>',
+     template: '你确定要退出应用吗?',
+     okText: '退出',
+     cancelText: '取消'
+     });
+     confirmPopup.then(function (res) {
+     if (res) {
+     ionic.Platform.exitApp();
+     } else {
+     // Don't close
+     }
+     });
+     }
+     //判断处于哪个页面时双击退出
+     if ($location.path() == '/tab' ) {
+     if (ionic.keyboard.isOpen) {
+     $cordovaKeyboard.close();
+     }else{
+     showConfirm();
+     }
+     }else if ($ionicHistory.backView()) {
+     if (ionic.keyboard.isOpen) {
+     $cordovaKeyboard.close();
+     }else{
+     $ionicHistory.goBack();
+     }
+     }else {
+     // This is the last page: Show confirmation popup
+     showConfirm();
+     }
+     e.preventDefault();
+     return false;
+     }, 101);
+     */
 })
 
 .config(function($stateProvider, $urlRouterProvider,$ionicConfigProvider) {
@@ -126,19 +127,41 @@ angular.module('starter', ['ionic','ngCordova','starter.controllers'])
   // setup an abstract state for the tabs directive
     .state('tab', {
     url: '/tab',
-    abstract: false,     //抽象页面，if true不能独立作为页面展示只能作为其他页面父页面，当加载子页面之前加载父页面。
+    abstract: true,     //抽象页面，不能独立作为页面展示只能作为其他页面父页面，当加载子页面之前加载父页面。
     templateUrl: 'cm/app/tabs.html'
   })
 
-    //“我”界面内的路由设置
-    .state('user', {
-      url: '/user/setting',
-      abstract: false,
-      templateUrl: 'cm/app/user/setting/main.html'
-    })
-
   // Each tab has its own nav history stack:
 
+  .state('tab.cook', {
+    url: '/cook',
+    views: {
+      'tab-cook': {
+        templateUrl: 'cm/app/cook/main.html',
+        controller: 'cookCtrl'
+      }
+    }
+  })
+
+  .state('tab.business', {
+      url: '/business',
+      views: {
+        'tab-business': {
+          templateUrl: 'cm/app/business/main.html',
+          controller: 'businessCtrl'
+        }
+      }
+    })
+
+  .state('tab.share', {
+      url: '/share',
+      views: {
+          'tab-share': {
+              templateUrl: 'cm/app/share/main.html',
+              controller: 'shareCtrl'
+          }
+      }
+  })
     /*.state('tab.chat-detail', {
       url: '/chats/:chatId',
       views: {
@@ -149,7 +172,24 @@ angular.module('starter', ['ionic','ngCordova','starter.controllers'])
       }
     })*/
 
+  .state('tab.user', {
+    url: '/user',
+    views: {
+      'tab-user': {
+        templateUrl: 'cm/app/user/main.html',
+        controller: 'userCtrl'
+      }
+    }
+  })
+
+  //“我”界面内的路由设置
+  .state('user-setting', {
+      url: '/setting',
+      templateUrl: 'cm/app/user/setting/main.html',
+      controller: 'userSettingCtrl'
+  })
+
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab');
+  $urlRouterProvider.otherwise('/tab/cook');
 
 });
