eb06bc3e884e554a2dfa30fab07bd1affce4e23d
dyxm <857230132@qq.com>
2016-03-21 22:37:15 +0800

    为user页面的子页设置路由

--- www/cm/app/user/js/user.js
+++ www/cm/app/user/js/user.js
@@ -1,2 +1,40 @@
-app.controller('userSettingCtrl', function ($scope, $ionicHistory, $state) {
+app
+  //设置
+  .controller('userSettingCtrl', function ($scope, $ionicHistory, $state) {
 })
+  //添加朋友
+  .controller('userAddFriendCtrl', function ($scope, $ionicHistory, $state) {
+  })
+  //我的食谱
+  .controller('userRecipesCtrl', function ($scope, $ionicHistory, $state) {
+  })
+  //我的收藏
+  .controller('userCollectionsCtrl', function ($scope, $ionicHistory, $state) {
+  })
+  //我的订单
+  .controller('userOrdersCtrl', function ($scope, $ionicHistory, $state) {
+  })
+  //我的饭局
+  .controller('userMealCtrl', function ($scope, $ionicHistory, $state) {
+  })
+  //优惠活动
+  .controller('userActivitiesCtrl', function ($scope, $ionicHistory, $state) {
+  })
+//积分兑换
+  .controller('userIntegralsCtrl', function ($scope, $ionicHistory, $state) {
+  })
+  //关于我们
+  .controller('userAboutUsCtrl', function ($scope, $ionicHistory, $state) {
+  })
+  //意见反馈
+  .controller('userSuggestionsCtrl', function ($scope, $ionicHistory, $state) {
+  })
+
+
+
+  //获取前一页的标题
+  //.controller('GetPrevTitleCtrl', function($scope, $ionicNavBarDelegate){
+  //  $scope.getPreviousTitle = function() {
+  //    return $ionicNavBarDelegate.getPreviousTitle();
+  //  };
+  //})
--- www/js/app.js
+++ www/js/app.js
@@ -183,11 +183,106 @@ angular.module('starter', ['ionic','ngCordova','starter.controllers'])
   })
 
   //“我”界面内的路由设置
-  .state('user-setting', {
-      url: '/setting',
-      templateUrl: 'cm/app/user/setting/main.html',
-      controller: 'userSettingCtrl'
-  })
+    //设置
+    .state('tab.setting', {
+        url: '/user/setting',
+        views: {
+          'tab-user': {
+            templateUrl: 'cm/app/user/setting/main.html',
+            controller: 'userSettingCtrl'
+          }
+        }
+    })
+    //添加朋友
+    .state('tab.addFriend', {
+      url: '/user/addFriend',
+      views: {
+        'tab-user': {
+          templateUrl: 'cm/app/user/addFriend/main.html',
+          controller: 'userAddFriendCtrl'
+        }
+      }
+    })
+    //我的食谱
+    .state('tab.recipes', {
+      url: '/user/recipes',
+      views: {
+        'tab-user': {
+          templateUrl: 'cm/app/user/recipes/main.html',
+          controller: 'userRecipesCtrl'
+        }
+      }
+    })
+    //我的收藏
+    .state('tab.collections', {
+      url: '/user/collections',
+      views: {
+        'tab-user': {
+          templateUrl: 'cm/app/user/collections/main.html',
+          controller: 'userCollectionsCtrl'
+        }
+      }
+    })
+    //我的订单
+    .state('tab.orders', {
+      url: '/user/orders',
+      views: {
+        'tab-user': {
+          templateUrl: 'cm/app/user/orders/main.html',
+          controller: 'userOrdersCtrl'
+        }
+      }
+    })
+    //我的饭局
+    .state('tab.meal', {
+      url: '/user/meal',
+      views: {
+        'tab-user': {
+          templateUrl: 'cm/app/user/meal/main.html',
+          controller: 'userMealCtrl'
+        }
+      }
+    })
+    //优惠活动
+    .state('tab.activities', {
+      url: '/user/activities',
+      views: {
+        'tab-user': {
+          templateUrl: 'cm/app/user/activities/main.html',
+          controller: 'userActivitiesCtrl'
+        }
+      }
+    })
+    //积分兑换
+    .state('tab.integrals', {
+      url: '/user/integrals',
+      views: {
+        'tab-user': {
+          templateUrl: 'cm/app/user/integrals/main.html',
+          controller: 'userIntegralsCtrl'
+        }
+      }
+    })
+    //关于我们
+    .state('tab.aboutUs', {
+      url: '/user/aboutUs',
+      views: {
+        'tab-user': {
+          templateUrl: 'cm/app/user/aboutUs/main.html',
+          controller: 'userAboutUsCtrl'
+        }
+      }
+    })
+    //意见反馈
+    .state('tab.suggestions', {
+      url: '/user/suggestions',
+      views: {
+        'tab-user': {
+          templateUrl: 'cm/app/user/suggestions/main.html',
+          controller: 'userSuggestionsCtrl'
+        }
+      }
+    })
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/cook');
